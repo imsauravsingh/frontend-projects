b3510b9f516cc8c37a722f43a4b1cd76302267ed
imsauravsingh <imsauravsingh@gmail.com>
2024-08-20 00:24:04 +0530

    socket server configured to broadcast changes

--- google-docs-clone/client/src/TextEditor.tsx
+++ google-docs-clone/client/src/TextEditor.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io, Socket } from "socket.io-client";
 
 interface WrapperInterface {
   node: HTMLDivElement;
@@ -10,6 +11,47 @@ interface WrapperInterface {
 
 export default function TextEditor() {
   const quillRef = useRef<Quill | null>(null);
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const [quill, setQuill] = useState<Quill | undefined>(undefined);
+
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta: any) => {
+      console.log("receive changes----", delta);
+
+      quill.updateContents(delta);
+    };
+
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket?.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta: any, oldDelta: any, source: string) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill?.off("text-change", handler);
+    };
+  }, [socket, quill]);
 
   const wrapperRef = useCallback((instance: HTMLDivElement | null): void => {
     if (instance == null || quillRef.current != null) return;
@@ -27,9 +69,10 @@ export default function TextEditor() {
 
     wrapper.append(editor);
     // Initialize Quill editor only once
-    quillRef.current = new Quill(editor, {
+    const q = (quillRef.current = new Quill(editor, {
       theme: "snow",
-    });
+    }));
+    setQuill(q);
   }, []);
 
   return <div className="editor-container" ref={wrapperRef}></div>;
